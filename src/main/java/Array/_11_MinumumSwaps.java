57207270db9dbadbe61662f24d2fb356498c8cd0
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2026-03-18 18:11:23 -0400

    Update _11_MinumumSwaps.java

--- src/main/java/Array/_11_MinumumSwaps.java
+++ src/main/java/Array/_11_MinumumSwaps.java
@@ -1,4 +1,6 @@
-package Array;//Given an array arr and a number k. One can apply a swap operation on the array any number of times, i.e choose any two index i and j (i < j) and swap arr[i] , arr[j] .
+package Array;
+
+//Given an array arr and a number k. One can apply a swap operation on the array any number of times, i.e choose any two index i and j (i < j) and swap arr[i] , arr[j] .
 //Find the minimum number of swaps required to bring all the numbers less than or equal to k together,
 //i.e. make them a contiguous subarray.
 
