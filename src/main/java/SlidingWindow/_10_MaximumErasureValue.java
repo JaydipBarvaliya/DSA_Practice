f11eb5be615b59c83333e1330094f3fcbaf5131f
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2026-05-17 16:10:11 -0400

    Refactored

--- src/main/java/SlidingWindow/_10_MaximumErasureValue.java
+++ src/main/java/SlidingWindow/_10_MaximumErasureValue.java
@@ -1,9 +1,38 @@
 package SlidingWindow;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class _10_MaximumErasureValue {
 
-    public int maximumUniqueSubarray(int[] nums) {
-        return 0;
+    public int maximumUniqueSubarray(int[] arr) {
+
+        int i = 0;
+        int j = 0;
+        int windowSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        Set<Integer> set = new HashSet<>();
+
+        while (j < arr.length) {
+
+            windowSum = windowSum + arr[j];
+
+            if (set.contains(arr[j])) { //duplicate
+
+                while (arr[i] != arr[j]) {
+                    windowSum = windowSum - arr[i];
+                    set.remove(arr[i]);
+                    i++;
+                }
+                windowSum = windowSum - arr[i];
+                i++;
+            } else {
+                set.add(arr[j]);
+            }
+            j++;
+            maxSum = Math.max(maxSum, windowSum);
+        }
+        return maxSum;
     }
 
 }
